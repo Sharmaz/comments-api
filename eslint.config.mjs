b268f4a6fa59938143781ce1463a49e3a9e70cb9
Ivan Robles <irae45@gmail.com>
2024-10-27 17:18:55 -0600

    add tests end to end.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default [
     "**/src/db/migrations",
     "**/src/db/seeders",
     "**/jest.config.unit.js",
+    "**/jest.config.e2e.js",
     "**/dist/**"
   ]},
   {files: ["**/*.{js,mjs,cjs,ts}"]},
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-import express from 'express';
-import cors from 'cors';
+import createApp from './app';
 
-import routerApi from './routes/index';
-
-const router = express.Router();
 const PORT = process.env.PORT || 3000;
-
-router.get('/', async (req, res) => {
-  res.send('Hello World!');
-});
-
-const createApp = () => {
-  const app = express();
-  app.use(express.json());
-  app.use(cors());
-  app.use(router);
-
-  routerApi(app);
-
-  return app;
-};
-
 const app = createApp();
 
 app.listen(PORT, () => {
